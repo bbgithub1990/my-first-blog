7e57feebf0e8999fd3d4b5bdf1d816b8884ba18a
Briony Beaumont <uqbbeaum@eait.uq.edu.au>
2019-07-13 15:26:19 +1000

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,6 @@
+def hi(name):
+    print('Hi ' + name + '!')
+girls = ['Rachael', 'Dom', 'Brittany', 'Alisha', 'Amelia', 'Anneleis', 'Emma', 'Bry']
+for name in girls:
+    hi(name)
+    print('Next girl')
